c319e4eecc9f3d9a7f8c4a5afb9a064e5e99eba6
salmaelhajraoui <salma.hajrawi@gmail.com>
2021-09-10 17:56:20 +0200

    fix eslint stuff

--- src/router.js
+++ src/router.js
@@ -5,16 +5,16 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/coaches' },
     { path: 'coaches/', component: null },
-    { 
-      path: 'coaches/:id', 
-      component: null, 
+    {
+      path: 'coaches/:id',
+      component: null,
       children: [
         { path: 'contact', component: null },
-      ] 
+      ]
     },
     { path: 'register/', component: null },
     { path: 'requests/', component: null },
-    { path: '/:notFound(.*)', component:null },
+    { path: '/:notFound(.*)', component: null },
   ]
 })
 
